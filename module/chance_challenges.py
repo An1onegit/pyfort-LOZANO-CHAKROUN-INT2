fcaf028ba9eebb5b73aad70f5e25bd5547e9d841
An1onegit <linolozano75@gmail.com>
2024-12-05 19:19:41 +0100

    update

--- module/chance_challenges.py
+++ module/chance_challenges.py
@@ -10,7 +10,7 @@ def chance_challenge():
 
 def shell_game():
     l = ["A","B","C"]
-    print("The rules of this challenge are quite simple. you can chose between those three shells, if you find the key you win ! You have two tries quick!")
+    print("The rules of this challenge are quite simple. You can chose between those three shells, if you find the key you win ! You have two tries quick!")
     print(l)
     attempts = 0
     n = 0
@@ -20,7 +20,7 @@ def shell_game():
         shell = l[n]
         guess = input("Enter your guess : ")[0]
         if guess == shell :
-            print("Congrats ! you found the key. Seems like you are good at those.")
+            print("Congrats ! you found the key. Seems like you are good at these.")
             return True
         else :
             print("Wrong ! You have one more try.")
@@ -48,3 +48,5 @@ def roll_dice_game():
                 print("Next attempt")
     print("None of the players won... It's a draw.")
     return False
+
+chance_challenge()
--- module/math_challenges.py
+++ module/math_challenges.py
@@ -119,3 +119,4 @@ def math_roulette_challenge():
                 print("Incorrect answer!")
                 return False
 
+math_challenge()
